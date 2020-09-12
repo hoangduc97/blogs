8ed305ed0b8f2c7e2e6ec8353ca1f2492b5ad1b9
hoangduc97dn <hoangduc97dn@gmail.com>
2020-09-12 12:05:09 +0700

    UP

--- api/index.js
+++ api/index.js
@@ -1,34 +1,37 @@
 // import modules
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const {
     log,
     ExpressAPILogMiddleware
-} = require('@rama41222/node-logger')
+} = require("@rama41222/node-logger");
 
 // init server
-const app = express()
+const app = express();
 const logger = log({
     console: true,
     file: false,
-    label: 'blogs'
-})
+    label: "blogs",
+});
 // init port
-const PORT = process.env.PORT | 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, {
-    request: true
-}))
+    request: true,
+}));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     return res.json({
-        "say": "hello"
-    })
-})
+        hello: "duc",
+    });
+});
 
 app.listen(PORT, (error) => {
     console.log(`server running with port ${PORT}`);
-})
+});
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState("");
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    const fetchData = async() => {
+        const result = await axios.get('http://localhost:9000/');
+        setData(result.data['hello'])
+    };
+
+    return ( 
+        <div className = "App" >
+            {data}
+        </div>
+    );
 }
 
 export default App;
