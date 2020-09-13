03645faaf5ced382e5b3e5072109238eb568f597
hoangduc97dn <hoangduc97dn@gmail.com>
2020-09-13 10:35:18 +0700

    fix connect to db

--- api/index.js
+++ api/index.js
@@ -1,19 +1,11 @@
 // import modules
 const express = require("express");
 const cors = require("cors");
-const {
-    log,
-    ExpressAPILogMiddleware
-} = require("@rama41222/node-logger");
+const logger = require("morgan");
 const database = require('./src/config/database.config');
 
 // init server
 const app = express();
-const logger = log({
-    console: true,
-    file: false,
-    label: "blogs",
-});
 // init port
 const PORT = process.env.PORT || 3000;
 
@@ -22,19 +14,21 @@ app.use(express.urlencoded({
     extended: false
 }));
 app.use(cors());
-app.use(ExpressAPILogMiddleware(logger, {
-    request: true,
-}));
+app.use(logger('dev'));
 
 app.get("/", (req, res) => {
     return res.json({
         hello: "duc",
     });
 });
-
-database.connect_mongo(() => {
-    console.log('connect db success');
-});
+// connect to database
+database.connect_mongo()
+    .then(() => {
+        console.log('connect database success');
+    })
+    .catch((err) => {
+        console.error(err);
+    })
 
 app.listen(PORT, () => {
     console.log(`server running with port ${PORT}`);
--- api/src/config/database.config.js
+++ api/src/config/database.config.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_NAME = process.env.DB_NAME
-const DB_URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+const DB_URI = process.env.DB_URI
 
 const options = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 }
 
 const connect_mongo = () => {
-    console.log(DB_URI);
     return mongoose.connect(DB_URI, options)
 }
 
