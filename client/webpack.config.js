f4403cd8652a545758456f327017b8ea1f429a71
hoangduc97dn <hoangduc97dn@gmail.com>
2020-10-01 21:56:04 +0700

    UP

---
+++ client/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HWP = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, '/src/index.js'),
+    output: {
+        filename: 'build.js',
+        path: path.join(__dirname, '/dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HWP({
+            template: path.join(__dirname, '/src/index.html'),
+        }),
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        port: 80
+    },
+};
