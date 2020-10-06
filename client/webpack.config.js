6da9b42667a5e73f6c71d5c38e0c9fa6b3e3d12d
Truong Hoang Duc <truonghoangduc@luvina.net>
2020-10-06 09:51:12 +0700

    UP

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: path.join(__dirname, '/src/index.js'),
     output: {
         filename: 'build.js',
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, '/public'),
     },
     module: {
         rules: [
@@ -21,7 +21,7 @@ module.exports = {
     },
     plugins: [
         new HWP({
-            template: path.join(__dirname, '/src/index.html'),
+            template: path.join(__dirname, '/public/index.html'),
         }),
     ],
     devServer: {
