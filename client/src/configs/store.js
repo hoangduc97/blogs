6650116d81bce037c4b751591dbef061c43a9434
hoangduc97dn <hoangduc97dn@gmail.com>
2020-10-17 11:44:38 +0700

    remove epic and install redux-thunk

--- client/src/configs/store.js
+++ client/src/configs/store.js
@@ -1,4 +1,3 @@
-import { createEpicMiddleware } from 'redux-observable';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import rootReduder from './root.reducer';
@@ -6,9 +5,7 @@ import rootReduder from './root.reducer';
 // init middleware list
 const middlewares = [];
 
-const epicMiddleware = createEpicMiddleware();
 const logger = createLogger()
-middlewares.push(epicMiddleware);
 middlewares.push(logger);
 
 const store = createStore(rootReduder, applyMiddleware(...middlewares));
