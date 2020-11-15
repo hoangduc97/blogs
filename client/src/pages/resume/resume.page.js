a2570be6ab2e5fcd2700a5c71a6e136746e4688c
hoangduc97dn <hoangduc97dn@gmail.com>
2020-11-15 23:27:22 +0700

    update resume

--- client/src/pages/resume/resume.page.js
+++ client/src/pages/resume/resume.page.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { FaInbox, FaTerminal, FaProjectDiagram } from 'react-icons/fa';
+import {
+    FaInbox,
+    FaTerminal,
+    FaProjectDiagram,
+    FaPhoneAlt,
+} from 'react-icons/fa';
+import { ImLocation2, ImMail } from 'react-icons/im';
+import { AiOutlineMail } from 'react-icons/ai';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import * as data from './resume.data';
 import avatar from '../../assets/image/avatar.png';
 import './resume.scss';
@@ -43,6 +51,11 @@ function Resume() {
             </div>
             <div className="resume__avatar">
                 <img src={avatar} alt="Zrik" />
+                <div className="resume__author">
+                    <p className="name" data-text="DucTH">
+                        DucTH
+                    </p>
+                </div>
             </div>
             <div className="resume__detail">
                 <section className="resume__about" id="resume__about">
@@ -110,24 +123,78 @@ function Resume() {
                         <FaProjectDiagram className="resume__title--icon" />
                         <p className="resume__title--name">services</p>
                     </div>
-                    <ul className="resume__about__info">
-                        <li className="resume__about__info--item">23 years</li>
-                        <li className="resume__about__info--item">Vietnam</li>
-                        <li className="resume__about__info--item">Developer</li>
-                    </ul>
-                    <p className="resume__about__brief">
-                        Make things special and original on the web with Niko.
-                        If you are looking for the best way to present your
-                        skills, experience and services in the best possible
-                        light, get your hands on Niko. Kick things off by
-                        selecting the right samples, as well as dark or light
-                        look
-                    </p>
                 </section>
-                <section
-                    className="resume__contact"
-                    id="resume__contact"
-                ></section>
+                <section className="resume__contact" id="resume__contact">
+                    <div className="resume__title">
+                        <FaPhoneAlt className="resume__title--icon" />
+                        <p className="resume__title--name">contacts</p>
+                    </div>
+                    <div className="resume__contact--info">
+                        <p className="resume__contact--info--brief">
+                            If you have any questions regarding this privacy
+                            statement, the practices of this site or your
+                            dealings with me, please do not hesitate to contact
+                            at the above-mentioned address, e-mail address or
+                            phone number.
+                        </p>
+                        <LoadScript googleMapsApiKey="AIzaSyAX4aweIkqiXM_cNn05aDkEdn2XroDzoB0">
+                            <GoogleMap
+                                id="resume__map"
+                                zoom={8}
+                                center={{ lat: -34.397, lng: 150.644 }}
+                            ></GoogleMap>
+                            <Marker position={{ lat: -34.397, lng: 150.644 }} />
+                        </LoadScript>
+                        <div className="resume__contact--method">
+                            <div
+                                className="resume__contact--method--location"
+                                data-text="Danang, Vietnam"
+                            >
+                                <ImLocation2 />
+                            </div>
+                            <div
+                                className="resume__contact--method--phone"
+                                data-text="+84 862921702"
+                            >
+                                <FaPhoneAlt />
+                            </div>
+                            <div
+                                className="resume__contact--method--email"
+                                data-text="hoangduc97dn@gmail.com"
+                            >
+                                <AiOutlineMail />
+                            </div>
+                        </div>
+                    </div>
+                    <form
+                        className="resume__contact--form"
+                        action="mailto:hoangduc97dn@gmail.com"
+                        method="post"
+                        encType="text/plain"
+                    >
+                        <span className="resume__contact--title">send message</span>
+                        <input
+                            className="resume__contact--username"
+                            type="text"
+                            name="username"
+                            placeholder="user name"
+                        />
+                        <input
+                            className="resume__contact--email"
+                            type="email"
+                            name="email"
+                            placeholder="user email"
+                        />
+                        <textarea
+                            className="resume__contact--message"
+                            rows="4"
+                            cols="50"
+                            name="message"
+                            placeholder="content message"
+                        />
+                        <input className="resume__contact--button" type="submit" value="send" />
+                    </form>
+                </section>
             </div>
         </div>
     );
